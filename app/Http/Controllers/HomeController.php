147d7afab97e718ba5130d67db958a5195fcd0df
Angel Aguirre <angelaguirre.personal@gmail.com>
2020-10-26 20:13:48 -0600

    Muestra productos recientes en el home y la imagen del banner

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Book;
 
 class HomeController extends Controller
 {
@@ -27,6 +28,8 @@ class HomeController extends Controller
 
         $categories = Category::all();
 
-        return view('home', compact('categories'));
+        $books = Book::latest()->take(8)->get();
+
+        return view('home', compact('categories', 'books'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,4 +24,41 @@
 </div>
 <!-- End Main Banner Area -->
 
+<!-- Start Trending Products Area -->
+<section class="trending-products-area pb-60">
+    <div class="container" style="margin-top: 40px">
+        <div class="section-title without-bg">
+            <h2><span class="dot"></span> Libros Recientes</h2>
+        </div>
+
+        <div class="row">
+            <div class="trending-products-slides owl-carousel owl-theme">
+                @foreach ($books as $book)
+                    <div class="col-lg-12 col-md-12">
+                        <div class="single-product-box">
+                            <div class="product-image">
+                                <a href="#">
+                                    <img src="{{ asset('img/books/' . $book->image) }}" alt="image">
+                                    <img src="{{ asset('img/books/' . $book->image) }}" alt="image">
+                                </a>
+                            </div>
+
+                            <div class="product-content">
+                                <h3><a href="#">{{ $book->name }}</a></h3>
+
+                                <div class="product-price">
+                                    <span class="new-price">${{ $book->price }} MXN</span>
+                                </div>
+
+                                <a href="#" class="btn btn-light">Agregar al carrito</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Trending Products Area -->
+
 @endsection
